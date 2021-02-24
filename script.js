46312ec0503555ec1ed688c7725e12622fc65687
Tom S <tomalexs2004@gmail.com>
2021-02-24 22:06:51 +0000

    Changed to fully api based emotes to allow for easy change

--- script.js
+++ script.js
@@ -68,48 +68,7 @@ document.head.appendChild(styleElement);
 
 //#region  twitchEmotes
 
-let emoteIndex = new Map([
-    // ["emote name", "emote image url"]
-
-    // Twitch Emotes
-    ["KappaPride", "https://static-cdn.jtvnw.net/emoticons/v1/55338/1.0"],
-    ["Kappa", "https://static-cdn.jtvnw.net/emoticons/v2/25/default/dark/1.0"],
-    ["4Head", "https://static-cdn.jtvnw.net/emoticons/v1/354/1.0"],
-    ["BOP", "https://static-cdn.jtvnw.net/emoticons/v1/301428702/1.0"],
-    ["BabyRage", "https://static-cdn.jtvnw.net/emoticons/v1/22639/1.0"],
-    ["BibleThump", "https://static-cdn.jtvnw.net/emoticons/v1/86/1.0"],
-    ["DarkMode", "https://static-cdn.jtvnw.net/emoticons/v1/461298/1.0"],
-    ["HeyGuys", "https://static-cdn.jtvnw.net/emoticons/v1/30259/1.0"],
-    ["Keepo", "https://static-cdn.jtvnw.net/emoticons/v1/1902/1.0"],
-    ["KonCha", "https://static-cdn.jtvnw.net/emoticons/v1/160400/1.0"],
-    ["LUL", "https://static-cdn.jtvnw.net/emoticons/v1/425618/1.0"],
-    ["NotLikeThis", "https://static-cdn.jtvnw.net/emoticons/v1/58765/1.0"],
-    ["ResidentSleeper", "https://static-cdn.jtvnw.net/emoticons/v1/245/1.0"],
-    ["SMOrc", "https://static-cdn.jtvnw.net/emoticons/v1/52/1.0"],
-    ["SSSsss", "https://static-cdn.jtvnw.net/emoticons/v1/46/1.0"],
-    ["yes","https://cdn.betterttv.net/emote/5fc7db308c4e2c46195cc009/3x"],
-
-    // BTTV emotes
-    ["monkaS", "https://cdn.betterttv.net/emote/56e9f494fff3cc5c35e5287e/3x"],
-    ["POGGERS", "https://cdn.betterttv.net/emote/58ae8407ff7b7276f8e594f2/3x"],
-    ["PepeHands", "https://cdn.betterttv.net/emote/59f27b3f4ebd8047f54dee29/3x"],
-    ["Pepega", "https://cdn.betterttv.net/emote/5aca62163e290877a25481ad/3x"],
-    ["KEKW", "https://cdn.betterttv.net/emote/5e9c6c187e090362f8b0b9e8/3x"],
-    ["5Head", "https://cdn.betterttv.net/emote/5d6096974932b21d9c332904/3x"],
-    ["AYAYA", "https://cdn.betterttv.net/emote/58493695987aab42df852e0f/3x"],
-    ["Sadge", "https://cdn.betterttv.net/emote/5e0fa9d40550d42106b8a489/3x"],
-    ["peepoHappy", "https://cdn.betterttv.net/emote/5a16ee718c22a247ead62d4a/3x"],
-    ["weSmart", "https://cdn.betterttv.net/emote/589771dc10c0975495c578d1/3x"],
-    ["POGSLIDE", "https://cdn.betterttv.net/emote/5aea37908f767c42ce1e0293/3x"],
-
-    // FFZ Emotes
-    ["Pog", "https://cdn.frankerfacez.com/emoticon/210748/2"],
-    ["pog", "https://cdn.frankerfacez.com/emoticon/210748/2"],
-    ["REEeee", "https://cdn.frankerfacez.com/emoticon/116831/1"],
-    ["Thonk", "https://cdn.frankerfacez.com/emoticon/191246/2"],
-    ["Stonks", "https://cdn.frankerfacez.com/emoticon/428011/2"],
-
-]);
+let emoteIndex = new Map();
 
 fetch("https://api.betterttv.net/3/emotes/shared/top?offset=0&limit=100").then(response => response.json()).then(data => {
     data.forEach(element => {
@@ -121,7 +80,7 @@ fetch("https://api.betterttv.net/3/emotes/shared/top?offset=0&limit=100").then(r
 
 fetch("https://twitchmeet.tomsteer.me/emotes").then(response => response.json()).then(data => {
     data.forEach(element => {
-        let src = "https://cdn.betterttv.net/emote/" + element.emote.id + "/3x";
+        let src = element.emote.src;
         let name = element.emote.code;
         emoteIndex.set(name,src)
     })
